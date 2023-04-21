a8db63672ea9a35b49b5ba14b9d843406a4800ae
Gaku-tohoku <xueyueshanghai@hotmail.com>
2023-04-21 11:03:39 +0800

    8.5 示例学习：多选题测验评分

--- src/Chapter 8/PassTwoDimensionalArray_8_1.java
+++
@@ -1,2 +0,0 @@
-package PACKAGE_NAME;public class PassTwoDimensionalArray_8_1 {
-}
---
+++ src/Chapter 8/src/PassTwoDimensionalArray_8_1.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+
+public class PassTwoDimensionalArray_8_1 {
+    public static void main(String[] args) {
+        int[][] m = getArray(); // Get an array
+    }
+}
